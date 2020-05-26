1dde54ad8fc2e49a1d3cde933030316cfec46098
osidekyle <hawkinskyle67@gmail.com>
2020-05-25 17:55:47 -0700

    code for computation, choosing operators, delete

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ class Calculator{
     }
     delete(){
 
+        this.currentOperand=this.currentOperand.toString().slice(0,-1);
     }
     appendNumber(number){
         if(number==="." && this.currentOperand.includes(".")) return;
@@ -19,13 +20,39 @@ class Calculator{
 
     }
     chooseOperation(operation){
+        if(this.currentOperand===" ")return;
+        if(this.previousOperand !== " "){
+            this.compute();
+        }
         this.operation=operation;
         this.previousOperand=this.currentOperand;
         this.currentOperand='';
 
     }
     compute(){
-
+        let computation
+        let prev = parseFloat(this.previousOperand);
+        let current= parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(current)) return;
+        switch (this.operation){
+            case '+':
+                computation=prev+current;
+                break
+            case '-':
+                computation=prev-current;
+                break
+            case '*':
+                computation=prev*current;
+                break
+            case "÷":
+                computation=prev/current;
+                break
+            default:
+                return
+        }
+        this.currentOperand=computation;
+        this.operation=undefined;
+        this.previousOperand=" ";
     }
     updateDisplay(){
         this.currentOperandTextElement.innerText=this.currentOperand;
@@ -60,4 +87,19 @@ operationButtons.forEach(button=>{
         calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
+})
+
+equalsButton.addEventListener('click', button =>{
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+
+allClearButton.addEventListener('click', button =>{
+    calculator.clear();
+    calculator.updateDisplay();
+})
+deleteButton.addEventListener('click', button =>{
+    calculator.delete();
+    calculator.updateDisplay();
 })
